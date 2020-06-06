a09d54340442e19260260c024af623e33904f33f
Aero51 <nsrdoc@yahoo.com>
2020-06-06 03:36:38 +0200

    Extracted retrofit call and response to its own method

--- app/src/main/java/com/aero51/moviedatabase/repository/TopRatedResultDataSource.java
+++ app/src/main/java/com/aero51/moviedatabase/repository/TopRatedResultDataSource.java
@@ -4,12 +4,10 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.paging.PageKeyedDataSource;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 public class TopRatedResultDataSource extends PageKeyedDataSource<Integer, Top_Rated_Result> {
@@ -21,31 +19,14 @@ public class TopRatedResultDataSource extends PageKeyedDataSource<Integer, Top_R
     public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, Top_Rated_Result> callback) {
         TheMovieDbApi theMovieDbApi = RetrofitInstance.getApiService();
         Call<Top_Rated_Movies_Page> call = theMovieDbApi.getTopRatedMovies(API_KEY, TOP_RATED_MOVIES_FIRST_PAGE);
-
-        try {
-            Response<Top_Rated_Movies_Page> response = call.execute();
-            if (!response.isSuccessful()) {
-
-                Log.d("moviedatabaselog", "Response unsuccesful: " + response.code());
-                return;
-            }
-            Top_Rated_Movies_Page mTopRatedMovies = response.body();
-            Log.d("moviedatabaselog", "load initial ");
-            List<Top_Rated_Result> list_of_results = mTopRatedMovies.getResults_list();
-            callback.onResult(list_of_results, null, TOP_RATED_MOVIES_FIRST_PAGE + 1);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("moviedatabaselog", "call failure loadInitial  IOException : " + e.getMessage());
-        }
-
+        Log.d("moviedatabaselog", "load initial ");
+        List<Top_Rated_Result> list_of_results= fetchTopRatedMovies(call);
+        callback.onResult(list_of_results,null,TOP_RATED_MOVIES_FIRST_PAGE+1);
     }
 
     @Override
     public void loadBefore(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Top_Rated_Result> callback) {
-
         Log.d("moviedatabaselog", "Load before: " + params.key);
-
     }
 
     @Override
@@ -53,23 +34,27 @@ public class TopRatedResultDataSource extends PageKeyedDataSource<Integer, Top_R
         TheMovieDbApi theMovieDbApi = RetrofitInstance.getApiService();
         Call<Top_Rated_Movies_Page> call = theMovieDbApi.getTopRatedMovies(API_KEY, params.key);
 
+        Log.d("moviedatabaselog", "load after ");
+        List<Top_Rated_Result> list_of_results= fetchTopRatedMovies(call);
+            callback.onResult(list_of_results, params.key + 1);
+    }
+
+    private List<Top_Rated_Result> fetchTopRatedMovies(Call<Top_Rated_Movies_Page> call){
+        List<Top_Rated_Result> list_of_results = new ArrayList<>();
         try {
             Response<Top_Rated_Movies_Page> response = call.execute();
             if (!response.isSuccessful()) {
-
                 Log.d("moviedatabaselog", "Response unsuccesful: " + response.code());
-                return;
+                return null;
             }
             Top_Rated_Movies_Page mTopRatedMovies = response.body();
-
-            List<Top_Rated_Result> list_of_results = mTopRatedMovies.getResults_list();
-            //incrementing the next page number
-            Log.d("moviedatabaselog", "Load after:params key:  " + params.key);
-            callback.onResult(list_of_results, params.key + 1);
+            Log.d("moviedatabaselog", "load initial ");
+             list_of_results = mTopRatedMovies.getResults_list();
         } catch (IOException e) {
             e.printStackTrace();
-            Log.d("moviedatabaselog", "call failure loadAfter  IOException : " + e.getMessage());
+            Log.d("moviedatabaselog", "call failure  IOException : " + e.getMessage());
         }
+        return list_of_results;
 
     }
 
