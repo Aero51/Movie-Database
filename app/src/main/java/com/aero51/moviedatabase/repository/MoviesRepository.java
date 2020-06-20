d9b0e023370192854b9ae86269bf255dbcba85dd
Aero51 <nsrdoc@yahoo.com>
2020-06-20 18:41:00 +0200

    Refactored Top rated movies repository and popular movies repository into single MoviesRepository

---
+++ app/src/main/java/com/aero51/moviedatabase/repository/MoviesRepository.java
@@ -0,0 +1,85 @@
+package com.aero51.moviedatabase.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
+import com.aero51.moviedatabase.repository.db.MoviesDatabase;
+import com.aero51.moviedatabase.repository.db.PopularMoviesDao;
+import com.aero51.moviedatabase.repository.db.TopRatedMoviesDao;
+import com.aero51.moviedatabase.repository.model.NetworkState;
+import com.aero51.moviedatabase.repository.model.movie.PopularMovie;
+import com.aero51.moviedatabase.repository.model.movie.PopularMoviesPage;
+import com.aero51.moviedatabase.repository.model.movie.TopRatedMovie;
+import com.aero51.moviedatabase.repository.model.movie.TopRatedMoviesPage;
+import com.aero51.moviedatabase.utils.AppExecutors;
+
+public class MoviesRepository {
+    private LiveData<PagedList<TopRatedMovie>> topRatedMoviesPagedList;
+    private LiveData<PagedList<PopularMovie>> popularMoviesPagedList;
+    private TopRatedMoviesBoundaryCallback topRatedMoviesBoundaryCallback;
+    private PopularMoviesBoundaryCallback popularMoviesBoundaryCallback;
+    private AppExecutors executors;
+
+    public MoviesRepository(Application application, AppExecutors executors) {
+        this.executors = executors;
+        MoviesDatabase database = MoviesDatabase.getInstance(application);
+        TopRatedMoviesDao topRatedMoviesDao = database.get_top_rated_movies_dao();
+        PopularMoviesDao popularMoviesDao = database.get_popular_movies_dao();
+        topRatedMoviesBoundaryCallback = new TopRatedMoviesBoundaryCallback(application, executors);
+        popularMoviesBoundaryCallback = new PopularMoviesBoundaryCallback(application, executors);
+        createTopRatedMoviesPagedList(topRatedMoviesDao);
+        createPopularMoviesPagedList(popularMoviesDao);
+
+    }
+    private PagedList.Config getPagedListConfig(){
+        return  (new PagedList.Config.Builder())
+                .setEnablePlaceholders(false)
+                .setPrefetchDistance(40)
+                .setInitialLoadSizeHint(60)
+                .setPageSize(20).build();
+    }
+
+    private void createTopRatedMoviesPagedList(TopRatedMoviesDao dao){
+        PagedList.Config pagedListConfig = getPagedListConfig();
+        topRatedMoviesPagedList = new LivePagedListBuilder<>(dao.getAllResults(), pagedListConfig)
+                .setBoundaryCallback(topRatedMoviesBoundaryCallback).setFetchExecutor(executors.networkIO())
+                .build();
+
+    }
+    private void createPopularMoviesPagedList(PopularMoviesDao dao) {
+        PagedList.Config pagedListConfig = getPagedListConfig();
+        popularMoviesPagedList = new LivePagedListBuilder<>(dao.getAllResults(), pagedListConfig)
+                .setBoundaryCallback(popularMoviesBoundaryCallback).setFetchExecutor(executors.networkIO())
+                .build();
+    }
+
+
+
+    public LiveData<PagedList<TopRatedMovie>> getTopRatedResultsPagedList() {
+        return topRatedMoviesPagedList;
+    }
+
+    public LiveData<NetworkState> getTopRatedNetworkState() {
+        return topRatedMoviesBoundaryCallback.getNetworkState();
+    }
+
+    public LiveData<TopRatedMoviesPage> getLastTopRatedMoviePage() {
+        return topRatedMoviesBoundaryCallback.getCurrent_movie_page();
+    }
+
+    public LiveData<PagedList<PopularMovie>> getPopularResultsPagedList() {
+        return popularMoviesPagedList;
+    }
+
+    public LiveData<NetworkState> getPopularNetworkState() {
+        return popularMoviesBoundaryCallback.getNetworkState();
+    }
+
+    public LiveData<PopularMoviesPage> getLastPopularMoviePage() {
+        return popularMoviesBoundaryCallback.getCurrent_movie_page();
+    }
+
+}
--- app/src/main/java/com/aero51/moviedatabase/viewmodel/MoviesViewModel.java
+++ app/src/main/java/com/aero51/moviedatabase/viewmodel/MoviesViewModel.java
@@ -8,58 +8,58 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.paging.PagedList;
 
-import com.aero51.moviedatabase.repository.PopularMoviesRepository;
+import com.aero51.moviedatabase.repository.MoviesRepository;
 import com.aero51.moviedatabase.repository.model.NetworkState;
 import com.aero51.moviedatabase.repository.model.movie.PopularMovie;
 import com.aero51.moviedatabase.repository.model.movie.PopularMoviesPage;
 import com.aero51.moviedatabase.repository.model.movie.TopRatedMovie;
 import com.aero51.moviedatabase.repository.model.movie.TopRatedMoviesPage;
-import com.aero51.moviedatabase.repository.TopRatedMoviesRepository;
 import com.aero51.moviedatabase.utils.AppExecutors;
 
-
 public class MoviesViewModel extends AndroidViewModel {
-    private TopRatedMoviesRepository topRatedMoviesRepository;
-    private PopularMoviesRepository popularMoviesRepository;
+
+    private MoviesRepository moviesRepository;
     private AppExecutors executors;
 
     public MoviesViewModel(@NonNull Application application) {
         super(application);
-        executors=new AppExecutors();
-        topRatedMoviesRepository = new TopRatedMoviesRepository(application,executors);
-        popularMoviesRepository= new PopularMoviesRepository(application,executors);
+        executors = new AppExecutors();
+        moviesRepository = new MoviesRepository(application, executors);
     }
 
+
     public LiveData<PagedList<TopRatedMovie>> getTopRatedResultsPagedList() {
-        return topRatedMoviesRepository.getTopRatedResultsPagedList();
+        return moviesRepository.getTopRatedResultsPagedList();
     }
+
     //used to get the page number by adding observer
     public LiveData<TopRatedMoviesPage> getTopRatedLiveMoviePage() {
-        return topRatedMoviesRepository.getCurrent_movie_page();
+        return moviesRepository.getLastTopRatedMoviePage();
     }
 
     public LiveData<NetworkState> getTopRatedMoviesNetworkState() {
-        return topRatedMoviesRepository.getNetworkState();
+        return moviesRepository.getTopRatedNetworkState();
     }
-
 
 
     public LiveData<PagedList<PopularMovie>> getPopularResultsPagedList() {
-        return popularMoviesRepository.getPopularResultsPagedList();
+        return moviesRepository.getPopularResultsPagedList();
     }
+
     //used to get the page number by adding observer
     public LiveData<PopularMoviesPage> getPopularLiveMoviePage() {
-        return popularMoviesRepository.getCurrent_movie_page();
+        return moviesRepository.getLastPopularMoviePage();
     }
+
     public LiveData<NetworkState> getPopularMoviesNetworkState() {
-        return popularMoviesRepository.getNetworkState();
+        return moviesRepository.getPopularNetworkState();
     }
 
 
     @Override
     protected void onCleared() {
         Log.d("moviedatabaselog", "view model on cleared ");
-       // repository.getMoviePageLd().removeObserver(repository.getObserver());
+        // repository.getMoviePageLd().removeObserver(repository.getObserver());
         super.onCleared();
     }
 
